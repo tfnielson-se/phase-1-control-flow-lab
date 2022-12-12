f90ad749c00eddd2b9b414f0745d021f0ca7dcf5
Thomas <tfnielson.se@gmail.com>
2022-12-12 16:38:04 -0700

    commit tree ae47058f7c14ea8b5a89b973649558a02715a4c2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet() {
+	// write code here
 }
+
 
 function ternaryCheckCity(){
   // Write your code here!
@@ -9,3 +10,16 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+
+function exerciseDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+
+exerciseDog("Boo", "puggle");
+exerciseDog("Jojo", "mutt");
+exerciseDog("Emmeline", "bernedoodle");
